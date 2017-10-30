c82ec8b3b332c7ad53c5b9b21c1ae395c4357a4a
lei.du <lei.du@mobvista.com>
2017-10-30 19:17:24 +0800

    add scala MainTest

---
+++ Scala/src/MainTest.scala
@@ -0,0 +1,11 @@
+object MainTest {
+  def add(x : Int, y : Int): Int = {
+    x + y;
+  }
+  println(add(1,2))
+  println("hello scala")
+
+  def main(args : Array[String]){
+    println(add(1,2))
+  }
+}
